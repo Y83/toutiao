2cfaa865d6b102c670cb10e573bc2888bdd2ddbc
Y83 <70987929+Y83@users.noreply.github.com>
2021-03-28 15:28:10 +0800

    Delete js directory

--- js/chunk-2d0a2e01.0875c969.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a2e01"],{"007b":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"home-container"},[e._v(" 首页 ")])},c=[],o={name:"HomeIndex",data:function(){return{}},created:function(){},methods:{}},r=o,s=t("2877"),u=Object(s["a"])(r,a,c,!1,null,"d68c7fa0",null);n["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d0a2e01.0875c969.js.map
--- js/chunk-2d217eac.0babee0f.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d217eac"],{c988:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"home-container"},[e._v(" 首页 ")])},a=[],o={name:"HomeIndex",data:function(){return{}},created:function(){},methods:{}},r=o,s=t("2877"),u=Object(s["a"])(r,c,a,!1,null,"36f9f012",null);n["default"]=u.exports}}]);
-//# sourceMappingURL=chunk-2d217eac.0babee0f.js.map
